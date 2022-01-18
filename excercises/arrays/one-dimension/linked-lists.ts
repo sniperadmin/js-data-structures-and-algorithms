b7706a28eb458a2d2356d926bc04f71d4894848c
Nasr Galal <nasrbue@gmail.com>
2022-01-18 02:11:21 +0200

    feat: add exercise on circular singly linked list

--- excercises/arrays/one-dimension/linked-lists.ts
+++ excercises/arrays/one-dimension/linked-lists.ts
@@ -6,26 +6,29 @@
  */
 class SingleNode {
   data: number
+  prev: SingleNode | null;
   next: SingleNode | null;
 
   constructor(data: number) {
     this.data = data;
+    this.prev = null;
     this.next = null;
   }
 }
 
-
 /**
- * @class SinglyLinkedList
- * Defines a single linked
- * @param {O} head defines the anchor head node
- * @param {O} tail defines the anchor tail node
+ * Defines a single LinkedList
+ * @class LinkedList
+ * @param {SingleNode} head defines the anchor head node
+ * @param {SingleNode} tail defines the anchor tail node
+ * @param {SingleNode} current dynamic pointer (used for some internal indexing operations)
+ * @param {number} size defines the length of the List
  */
-class SinglyLinkedList {
-  private head: SingleNode | null;
-  private tail: SingleNode | null;
-  private current: SingleNode | null;
-  private size: number
+class LinkedList {
+  protected head: SingleNode | null;
+  protected tail: SingleNode | null;
+  protected current: SingleNode | null;
+  protected size: number
 
   constructor() {
     this.head = null;
@@ -34,6 +37,11 @@ class SinglyLinkedList {
     this.size = 0;
   }
 
+  /**
+   * @function getNode
+   * Used to capture the current node in the list
+   * @argument {number} index => the index of the desired node
+   */
   public getNode(index: number) {
     let node = this.head!
     for (let i = 0; i < index; i++) {
@@ -41,10 +49,21 @@ class SinglyLinkedList {
     }
     return node
   }
+}
 
-  public traverse() {
+
+/**
+ * @class SinglyLinkedList
+ * Defines a tyoe of LinkedList
+ */
+class SinglyLinkedList extends LinkedList {
+  /**
+   * @function traverse
+   * @returns {void}
+   */
+  public traverse(): void {
     if (!this.head) {
-      return 'Current list is Empty!'
+      throw new Error('Current list is Empty!')
     } else {
       this.current = this.head
       while (this.current) {
@@ -54,8 +73,12 @@ class SinglyLinkedList {
     }
   }
 
-  public search(searchVal: number) {
-    if (!this.head) return 'Current list is Empty!';
+  /**
+   * @function search
+   * @argument {number} searchVal
+   */
+  public search(searchVal: number): Error | number | null {
+    if (!this.head) throw new Error('Current list is Empty!');
     let index = 0
     this.current = this.head
     while (this.current) {
@@ -63,18 +86,18 @@ class SinglyLinkedList {
       index++;
       this.current = this.current.next;
     }
+    return this.current
   }
 
   /**
    * @function addNode
-   * @return {SingleNode}
    * @argument {number} data => The actual value of a node
+   * @returns {SingleNode}
    */
   public addNode(data: number): SingleNode {
     let newNode: SingleNode = new SingleNode(data);
     if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
+      this.head = this.tail = newNode;
     } else {
       this.tail!.next = newNode;
       this.tail = newNode;
@@ -83,6 +106,12 @@ class SinglyLinkedList {
     return newNode;
   }
 
+  /**
+   * @function addAtIndex
+   * @argument {number} data => The actual value of a node
+   * @argument {number} index => The index of the
+   * @returns {void}
+   */
   public addAtIndex(index: number, data: number) {
     if (index < 0 || index > this.size) return;
     if (index === 0) this.addAtHead(data)
@@ -96,7 +125,12 @@ class SinglyLinkedList {
     }
   }
 
-  public addAtHead(data: number) {
+  /**
+   * @function addAtHead
+   * @argument {number} data => The actual value of a node
+   * @returns {void}
+   */
+  public addAtHead(data: number): void {
     let newNode: SingleNode = new SingleNode(data);
     if (this.size === 0) {
       this.head = this.tail = newNode
@@ -107,7 +141,12 @@ class SinglyLinkedList {
     this.size++;
   }
 
-  public addAtTail(data: number) {
+  /**
+   * @function addAtIndex
+   * @argument {number} data => The actual value of a node
+   * @returns {void}
+   */
+  public addAtTail(data: number): void {
     let newNode: SingleNode = new SingleNode(data);
     if (this.size === 0) {
       this.head = this.tail = newNode
@@ -118,7 +157,12 @@ class SinglyLinkedList {
     this.size++;
   }
 
-  public deleteAtIndex(index: number) {
+  /**
+   * @function deleteAtIndex
+   * @argument {number} index => The index of a node
+   * @returns {void}
+   */
+  public deleteAtIndex(index: number): void {
     if (index < 0 || index >= this.size) throw new Error('index is way over than the list size!');
     if (index == 0) this.head = this.head!.next;
     else {
@@ -130,7 +174,11 @@ class SinglyLinkedList {
     this.size--
   }
 
-  public deleteEntireList() {
+  /**
+   * @function deleteEntireList
+   * @returns {void}
+   */
+  public deleteEntireList(): void {
     this.head = this.tail = null;
   }
 
@@ -148,9 +196,165 @@ class SinglyLinkedList {
   }
 }
 
+
+class CircularSinglyLinkedList extends LinkedList {
+  /**
+   * @function getNode
+   * Used to capture the current node in the list
+   * @argument {number} index => the index of the desired node
+   */
+  public addNode(data: number): void {
+    let newNode: SingleNode = new SingleNode(data);
+    if (!this.head) this.head = this.tail = newNode;
+    else {
+      this.tail!.next = newNode;
+      newNode.next = this.head;
+      this.tail = newNode;
+    }
+  }
+
+  /**
+   * @function traverse
+   * @returns {void}
+   */
+  public traverse(): void {
+    if (!this.head) throw new Error('Empty Circular List!');
+    console.log(this.head.data);
+    this.current = this.head.next
+
+    while (this.current !== this.head) {
+      console.log(this.current!.data);
+      this.current = this.current!.next;
+    }
+  }
+
+  /**
+   * @function search
+   * @argument {number} searchVal
+   * @returns {Error|number}
+   */
+  public search(searchVal: number): Error | number {
+    if (!this.head) throw new Error('Empty CircularSinglyLinkedList!');
+    if (this.head.data === searchVal) return 0;
+    let index = 1
+
+    this.current = this.head.next
+    while (this.current !== this.head) {
+      if (this.current!.data == searchVal) return index;
+      index++;
+      this.current = this.current!.next;
+    }
+    throw new Error("Search value not found!");
+  }
+
+  /**
+   * @function addAtHead
+   * @argument {number} data
+   * @returns {void}
+   */
+  public addAtHead(data: number): void {
+    let newNode: SingleNode = new SingleNode(data);
+    if (!this.head) {
+      this.head = this.tail = newNode;
+      this.tail.next = this.head
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+      this.tail!.next = newNode;
+    }
+    this.size++;
+  }
+
+  /**
+   * @function addAtTail
+   * @argument {number} data
+   * @returns {void}
+   */
+  public addAtTail(data: number): void {
+    let newNode: SingleNode = new SingleNode(data);
+    if (!this.head) {
+      this.head = this.tail = newNode;
+      this.tail.next = this.head
+    } else {
+      this.tail!.next = newNode;
+      newNode.next = this.head;
+      this.tail = newNode;
+    }
+    this.size++;
+  }
+
+
+  /**
+   * @function addAtIndex
+   * @argument {number} data
+   * @argument {number} index
+   * @returns {void}
+   */
+  public addAtIndex(index: number, data: number): void {
+    if (index < 0 || index >= this.size) throw new Error("Invalid index");
+    if (index == 0) this.addAtHead(data)
+    else if (index == this.size - 1) this.addAtTail(data)
+    else {
+      let newNode: SingleNode = new SingleNode(data);
+      let prev = this.getNode(index - 1);
+      prev.next = newNode;
+      newNode.next = prev.next;
+    }
+    this.size++;
+  }
+
+  /**
+   * @function deleteAtIndex
+   * @argument {number} index
+   * @returns {void}
+   */
+  public deleteAtIndex(index: number): void {
+    if (index < 0 || index >= this.size) throw new Error("Invalid index");
+    if (index == 0) this.head = this.head!.next;
+    else {
+      let prev = this.getNode(index - 1);
+      prev.next = prev.next!.next;
+      if (index == this.size - 1) this.tail = prev
+    }
+    this.size--;
+  }
+
+  /**
+   * @function deleteEntireList
+   * @returns {void}
+   */
+  public deleteEntireList() {
+    if (!this.tail) this.tail!.next = null;
+    this.head = this.tail = null;
+    this.size = 0
+  }
+
+  /**
+   * @function log
+   * Used to capture the current node in the list
+   * @argument {void}
+   */
+  public log() {
+    if (!this.head) throw new Error('CircularSinglyLinkedList is Empty!');
+    this.current = this.head.next
+
+    while (this.current !== this.head) {
+      console.log(this.current);
+      this.current = this.current!.next;
+    }
+  }
+}
+
+
+
+
+/**
+ * Trying classes
+ */
 const testList = new SinglyLinkedList()
 testList.addNode(4);
 testList.addNode(5);
+testList.getNode(2);
 testList.addAtHead(6);
 testList.addAtTail(10);
 testList.addAtIndex(1, 55);
